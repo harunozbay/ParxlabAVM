11fc11fbc0530f460a9f003fea7b2769710480ab
harunozbay <harunozbay@users.noreply.github.com>
2018-07-05 16:16:51 +0300

    İlk commit

---
+++ ParxlabAVM/Models/parkyeri.cs
@@ -0,0 +1,36 @@
+namespace ParxlabAVM.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("parxlab.parkyeri")]
+    public partial class parkyeri
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public parkyeri()
+        {
+            anatablo = new HashSet<anatablo>();
+            cihaz = new HashSet<cihaz>();
+        }
+
+        [Required]
+        [StringLength(45)]
+        public string parkadi { get; set; }
+
+        public int firmaid { get; set; }
+
+        [Key]
+        public int parkid { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<anatablo> anatablo { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<cihaz> cihaz { get; set; }
+
+        public virtual firma firma { get; set; }
+    }
+}
---
+++ ParxlabAVM/Models/yetki.cs
@@ -0,0 +1,28 @@
+namespace ParxlabAVM.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("parxlab.yetki")]
+    public partial class yetki
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public yetki()
+        {
+            yetkili = new HashSet<yetkili>();
+        }
+
+        [Column("yetki")]
+        [Required]
+        [StringLength(45)]
+        public string yetki1 { get; set; }
+
+        public int yetkiid { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<yetkili> yetkili { get; set; }
+    }
+}
