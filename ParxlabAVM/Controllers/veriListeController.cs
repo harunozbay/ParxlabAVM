1f0c22e225df111e1a0eaa49886d7b267381a615
cihat <cihat@DESKTOP-12O03CL>
2018-07-05 16:46:33 +0300

    Zaman aralığında parka giren toplam araçları ve harcanan toplam zamanı listeleyen fonksiyonlar eklendi.

---
+++ ParxlabAVM/Controllers/veriListeController.cs
@@ -0,0 +1,19 @@
+﻿using ParxlabAVM.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParxlabAVM.Controllers
+{
+    public class veriListeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            GrafikVeriOlusturucu olusturucu = new GrafikVeriOlusturucu();
+            return View(olusturucu.HaftalaraGöreGirenArac(1,new DateTime(2018,06,25,0,0,0),new DateTime(2018,07,05,23,59,0)));
+        }
+    }
+}
