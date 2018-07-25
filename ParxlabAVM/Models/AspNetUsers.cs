5e98ab45691aa760aeb5a8fa2fdbd1706ac3861b
cihat <cihatduman04@gmail.com>
2018-07-25 11:58:48 +0300

    kullanılan tablonun kullanici olduğu gösterildi.

--- ParxlabAVM/Models/AspNetUsers.cs
+++ ParxlabAVM/Models/AspNetUsers.cs
@@ -7,6 +7,7 @@ namespace ParxlabAVM.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
+    [Table("kullanici")]
     public partial class AspNetUsers
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
